e3c0606c9e1d13dd6bdbd39961c89334c4b8007d
AillemaCc <yzw1635994342@outlook.com>
2025-04-24 17:18:51 +0800

    创建项目基本目录结构； 创建项目数据库； 创建测试接口，测试SpringBoot+MybatisPLUS联调情况； 创建配置文件，联调数据库； 添加SecurityConfig，实现鉴权和放行

---
+++ Alist-framework/src/main/java/org/AList/mapper/TestMapper.java
@@ -0,0 +1,7 @@
+package org.AList.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.AList.domain.entity.Test;
+
+public interface TestMapper extends BaseMapper<Test> {
+}
---
+++ Alist-students/src/main/java/org/AList/AListStudentApplication.java
@@ -0,0 +1,15 @@
+package org.AList;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@MapperScan("org.AList.mapper")
+public class AListStudentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AListStudentApplication.class, args);
+    }
+}
