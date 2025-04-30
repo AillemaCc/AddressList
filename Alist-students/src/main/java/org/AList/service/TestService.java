a2c8f5822635a5316f462ab40a0ac7e1ca5e0a56
AillemaCc <yzw1635994342@outlook.com>
2025-04-30 14:42:18 +0800

    test：添加接口联调测试套组，测试mybatis+Springboot联调功能

---
+++ Alist-students/src/main/java/org/AList/service/TestService.java
@@ -0,0 +1,12 @@
+package org.AList.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.AList.domain.dao.entity.Test;
+
+/**
+ * 测试接口
+ */
+public interface TestService extends IService<Test> {
+
+}
+
