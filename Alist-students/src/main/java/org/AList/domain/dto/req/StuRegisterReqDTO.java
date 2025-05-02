603b6d52cb2110c87e276c7a75093c2e005d2b4b
AillemaCc <yzw1635994342@outlook.com>
2025-05-02 17:01:22 +0800

    feat: 开发学生注册请求实体

--- Alist-students/src/main/java/org/AList/domain/dto/req/StuRegisterReqDTO.java
+++ Alist-students/src/main/java/org/AList/domain/dto/req/StuRegisterReqDTO.java
@@ -1,7 +1,43 @@
 package org.AList.domain.dto.req;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 学生注册接口请求体
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StuRegisterReqDTO {
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 学号，唯一标识
+     */
+    private String studentId;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 加密密码
+     */
+    private String password;
 }
