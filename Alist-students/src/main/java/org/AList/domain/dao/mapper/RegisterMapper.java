5162aea236ed2b5ca3d715ae11f27c2dac3437a5
AillemaCc <yzw1635994342@outlook.com>
2025-05-02 17:24:56 +0800

    update: 更新注释

--- Alist-students/src/main/java/org/AList/domain/dao/mapper/RegisterMapper.java
+++ Alist-students/src/main/java/org/AList/domain/dao/mapper/RegisterMapper.java
@@ -3,5 +3,8 @@ package org.AList.domain.dao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.AList.domain.dao.entity.RegisterDO;
 
+/**
+ * 注册实体类持久层
+ */
 public interface RegisterMapper extends BaseMapper<RegisterDO> {
 }
