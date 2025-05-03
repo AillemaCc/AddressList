1f42398a6cf217e780350aa55808903fd63c0f55
AillemaCc <yzw1635994342@outlook.com>
2025-05-03 15:20:47 +0800

    feat: 构建注册审核实体类、持久层、请求类

---
+++ Alist-admin/src/main/java/org/AList/domain/dao/mapper/RegisterMapper.java
@@ -0,0 +1,10 @@
+package org.AList.domain.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.AList.domain.dao.entity.RegisterDO;
+
+/**
+ * 注册实体类持久层
+ */
+public interface RegisterMapper extends BaseMapper<RegisterDO> {
+}
---
+++ Alist-admin/src/main/java/org/AList/domain/dto/resp/AuditUserPageRespDTO.java
@@ -0,0 +1,60 @@
+package org.AList.domain.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 待审核用户返回参数响应实体
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuditUserPageRespDTO {
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 学号，唯一标识
+     */
+    private String studentId;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 加密密码
+     */
+    private String password;
+
+    /**
+     * 注册状态（0待审核，1通过，2拒绝）
+     */
+    private Integer status;
+
+    /**
+     * 拒绝备注
+     */
+    private String remark;
+
+    /**
+     * 注册凭证
+     */
+    private String registerToken;
+}
