18b4b3465c5fe1800fbb8e9ae96026c5efb04efb
AillemaCc <yzw1635994342@outlook.com>
2025-05-06 18:45:20 +0800

    feat: 开发用户通讯信息实体类、接口层、持久层、控制层

---
+++ Alist-students/src/main/java/org/AList/domain/dao/mapper/ContactMapper.java
@@ -0,0 +1,10 @@
+package org.AList.domain.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.AList.domain.dao.entity.ContactDO;
+
+/**
+ * 通讯信息持久层
+ */
+public interface ContactMapper extends BaseMapper<ContactDO> {
+}
