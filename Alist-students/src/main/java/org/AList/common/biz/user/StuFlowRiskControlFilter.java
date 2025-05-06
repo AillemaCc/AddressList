09bc71d8b98c4b6bf63f86e264ed3d42dc07d576
AillemaCc <yzw1635994342@outlook.com>
2025-05-06 14:58:33 +0800

    feat: 开发用户操作流量风控过滤器

---
+++ Alist-students/src/main/java/org/AList/common/biz/user/StuFlowRiskControlFilter.java
@@ -0,0 +1,62 @@
+package org.AList.common.biz.user;
+
+import com.alibaba.fastjson.JSON;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.AList.common.convention.exception.ClientException;
+import org.AList.common.convention.result.Results;
+import org.AList.config.UserFlowRiskControlConfiguration;
+import org.assertj.core.util.Lists;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Optional;
+
+import static org.AList.common.convention.errorcode.BaseErrorCode.FLOW_LIMIT_ERROR;
+
+/**
+ * 用户操作流量风控过滤器
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class StuFlowRiskControlFilter implements Filter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private final UserFlowRiskControlConfiguration userFlowRiskControlConfiguration;
+
+    private static final String USER_FLOW_RISK_CONTROL_LUA_SCRIPT_PATH = "src/main/resources/lua/user_flow_risk_control.lua";
+
+    @SneakyThrows
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) {
+        DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource(USER_FLOW_RISK_CONTROL_LUA_SCRIPT_PATH)));
+        redisScript.setResultType(Long.class);
+        String username = Optional.ofNullable(StuIdContext.getStudentId()).orElse("other");
+        Long result = null;
+        try {
+            result = stringRedisTemplate.execute(redisScript, Lists.newArrayList(username), Long.parseLong(userFlowRiskControlConfiguration.getTimeWindow()));
+        } catch (Throwable ex) {
+            log.error("执行用户请求流量限制LUA脚本出错", ex);
+            returnJson((HttpServletResponse) response, JSON.toJSONString(Results.failure(new ClientException(FLOW_LIMIT_ERROR))));
+        }
+        if (result == null || result > userFlowRiskControlConfiguration.getMaxAccessCount()) {
+            returnJson((HttpServletResponse) response, JSON.toJSONString(Results.failure(new ClientException(FLOW_LIMIT_ERROR))));
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        }
+    }
+}
