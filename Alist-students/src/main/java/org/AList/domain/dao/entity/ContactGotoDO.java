404cb3f6df2f4dbb80f5677c87e04200edb3f95e
AillemaCc <yzw1635994342@outlook.com>
2025-05-07 15:34:05 +0800

    feat: 开发通讯信息路由表

---
+++ Alist-students/src/main/java/org/AList/domain/dao/entity/ContactGotoDO.java
@@ -0,0 +1,33 @@
+package org.AList.domain.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.*;
+import org.AList.common.database.BaseDO;
+
+/**
+ * 通讯信息路由表实体
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ContactGotoDO extends BaseDO {
+    @TableId(type = IdType.AUTO)
+
+    /**
+     * 主键id
+     */
+    private Integer id;
+
+    /**
+     * 通讯录信息id
+     */
+    private String contactId;
+
+    /**
+     * 拥有该信息的学号
+     */
+    private String ownerId;
+}
