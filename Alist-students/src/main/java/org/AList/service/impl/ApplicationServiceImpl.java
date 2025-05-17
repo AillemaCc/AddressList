98db0624a25ae5b134a038cf246e1ef603abdcac
AillemaCc <yzw1635994342@outlook.com>
2025-05-17 16:56:06 +0800

    feat: 开发站内信发送部分的责任链校验模式

--- Alist-students/src/main/java/org/AList/service/impl/ApplicationServiceImpl.java
+++ Alist-students/src/main/java/org/AList/service/impl/ApplicationServiceImpl.java
@@ -10,6 +10,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.AList.common.biz.user.StuIdContext;
 import org.AList.common.convention.exception.ClientException;
+import org.AList.common.enums.StudentChainMarkEnum;
+import org.AList.designpattern.chain.AbstractChainContext;
 import org.AList.domain.dao.entity.ApplicationDO;
 import org.AList.domain.dao.entity.ContactGotoDO;
 import org.AList.domain.dao.entity.StudentFrameworkDO;
@@ -22,13 +24,8 @@ import org.AList.domain.dto.req.ApplicationSendQueryPageReqDTO;
 import org.AList.domain.dto.req.ApplicationYONReqDTO;
 import org.AList.domain.dto.resp.ApplicationQueryPageRespDTO;
 import org.AList.service.ApplicationService;
-import org.AList.service.bloom.StudentIdBloomFilterService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
-
-import static org.AList.common.enums.UserErrorCodeEnum.USER_NULL;
 
 /**
  * 站内信方法实现类
@@ -39,7 +36,8 @@ public class ApplicationServiceImpl extends ServiceImpl<ApplicationMapper, Appli
     private final StudentFrameWorkMapper studentFrameWorkMapper;
     private final ApplicationMapper applicationMapper;
     private final ContactGotoMapper contactGotoMapper;
-    private final StudentIdBloomFilterService studentIdBloomFilterService;
+    private final AbstractChainContext<ApplicationSendMsgReqDTO> abstractChainContext;
+
 
     /**
      * 发送站内信申请
@@ -50,39 +48,21 @@ public class ApplicationServiceImpl extends ServiceImpl<ApplicationMapper, Appli
     @Transactional(rollbackFor = Exception.class)
     @Override
     public void sendApplication(ApplicationSendMsgReqDTO requestParam) {
-        if(!studentIdBloomFilterService.contain(requestParam.getReceiver())){
-            throw new ClientException(USER_NULL);
-        }
-        if(Objects.equals(requestParam.getReceiver(), StuIdContext.getStudentId())){
-            throw new ClientException("您不能给自己发送申请");
-        }
-        LambdaQueryWrapper<StudentFrameworkDO> validQueryWrapper = Wrappers.lambdaQuery(StudentFrameworkDO.class)
-                .eq(StudentFrameworkDO::getStudentId, requestParam.getReceiver())
-                .eq(StudentFrameworkDO::getStatus, 1)
-                .eq(StudentFrameworkDO::getDelFlag, 0);
-        StudentFrameworkDO receiverDO = studentFrameWorkMapper.selectOne(validQueryWrapper);
-
-        if(receiverDO == null) {
-            throw new ClientException("收信人状态异常，无法添加到通讯录");
-        }
-        LambdaQueryWrapper<ApplicationDO> uniqueMsgQueryWrapper = Wrappers.lambdaQuery(ApplicationDO.class)
-                .eq(ApplicationDO::getSender, StuIdContext.getStudentId())
-                .eq(ApplicationDO::getReceiver, requestParam.getReceiver())
-                .ne(ApplicationDO::getStatus, 2)
-                .eq(ApplicationDO::getDelFlag, 0);
-        Long selectCount = applicationMapper.selectCount(uniqueMsgQueryWrapper);
-        if(selectCount > 1){
-            throw new ClientException("您已经给对方发送过申请，请等待对方处理请求");
-        }
+        abstractChainContext.handler(StudentChainMarkEnum.APPLICATION_SEND_FILTER.name(), requestParam);
         LambdaQueryWrapper<StudentFrameworkDO> senderWrapper = Wrappers.lambdaQuery(StudentFrameworkDO.class)
                 .eq(StudentFrameworkDO::getStudentId, StuIdContext.getStudentId());
         StudentFrameworkDO sender = studentFrameWorkMapper.selectOne(senderWrapper);
         String senderName=sender.getName();
+
+        LambdaQueryWrapper<StudentFrameworkDO> receiverWrapper = Wrappers.lambdaQuery(StudentFrameworkDO.class)
+                .eq(StudentFrameworkDO::getStudentId, requestParam.getReceiver());
+        StudentFrameworkDO receiver = studentFrameWorkMapper.selectOne(receiverWrapper);
+        String receiverName=receiver.getName();
         ApplicationDO applicationDO =ApplicationDO.builder()
                 .sender(StuIdContext.getStudentId())
                 .senderName(senderName)
                 .receiver(requestParam.getReceiver())
-                .receiverName(receiverDO.getName())
+                .receiverName(receiverName)
                 .content(requestParam.getContent())
                 .status(0)
                 .build();
