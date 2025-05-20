22d510ed16c0521a709ff17ae313d313a1e79453
AillemaCc <yzw1635994342@outlook.com>
2025-05-20 15:26:49 +0800

    update: 优化抽象异常体系

--- Alist-framework/src/main/java/org/AList/common/convention/exception/AbstractException.java
+++ Alist-framework/src/main/java/org/AList/common/convention/exception/AbstractException.java
@@ -8,11 +8,12 @@ import org.springframework.util.StringUtils;
 import java.util.Optional;
 
 /**
- * 抽象项目中三类异常体系，客户端异常、服务端异常以及远程服务调用异常
+ * 抽象项目中四类异常体系，客户端异常、服务端异常、用户端异常以及远程服务调用异常
  *
  * @see ClientException
  * @see ServiceException
  * @see RemoteException
+ * @see UserException
  */
 @Getter
 public abstract class AbstractException extends RuntimeException {
