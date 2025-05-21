862884ee090c0bfe59be62581fee822a3b2d2297
AillemaCc <yzw1635994342@outlook.com>
2025-05-21 15:29:58 +0800

    feat: 开发刷新accessToken接口

--- Alist-students/src/main/java/org/AList/controller/StuLoginController.java
+++ Alist-students/src/main/java/org/AList/controller/StuLoginController.java
@@ -2,14 +2,20 @@ package org.AList.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.AList.annotation.MyLog;
+import org.AList.common.convention.exception.UserException;
 import org.AList.common.convention.result.Result;
 import org.AList.common.convention.result.Results;
+import org.AList.domain.dto.req.StuLoginRefreshToken;
 import org.AList.domain.dto.req.StuLoginReqDTO;
+import org.AList.domain.dto.resp.RefreshTokenRespDTO;
 import org.AList.domain.dto.resp.StuLoginRespDTO;
 import org.AList.service.StuService;
+import org.AList.service.StuToken.TokenService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static org.AList.common.convention.errorcode.BaseErrorCode.USER_NOT_LOGGED;
 
 /**
  * 学生用户端登录接口Controller层
@@ -19,12 +25,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class StuLoginController {
     private final StuService stuService;
+    private final TokenService tokenService;
 
     /**
      * 学生登录接口
      *
      * @param requestParam 学生登录请求体
-     * @param request
      * @return 学生登录响应体
      */
     @PostMapping("/login")
@@ -48,13 +54,26 @@ public class StuLoginController {
     /**
      * 学生登出接口
      * @param studentId 学号
-     * @param token 登录分配的token
      * @return 无--直接进行存在判断并且删除
      */
     @DeleteMapping("/logout")
-    public Result<Void> logout(@RequestParam("studentId") String studentId,@RequestParam("token") String token) {
-        stuService.logout(studentId,token);
+    public Result<Void> logout(@RequestParam("studentId") String studentId,@RequestParam("token") String accessToken,@RequestParam("refreshToken") String refreshToken) {
+        stuService.logout(studentId,accessToken,refreshToken);
         return Results.success().setMessage("用户已登出");
+    }
+
+    /**
+     * 刷新Access Token接口
+     * @return 新的Access Token
+     */
+    @PostMapping("/refreshToken")
+    public Result<RefreshTokenRespDTO> refreshToken(@RequestBody StuLoginRefreshToken requestParam) {
+        try {
+            String newAccessToken = tokenService.refreshStudentAccessToken(requestParam.getStudentId(),requestParam.getRefreshToken());
+            return Results.success(new RefreshTokenRespDTO(newAccessToken));
+        } catch (Exception e) {
+            throw new UserException(USER_NOT_LOGGED);
+        }
     }
 
 }
