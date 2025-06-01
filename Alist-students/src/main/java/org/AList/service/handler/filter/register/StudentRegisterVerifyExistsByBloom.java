de9006524e2893646f24e5990a2b0218a0c02ef9
AillemaCc <yzw1635994342@outlook.com>
2025-06-01 21:43:16 +0800

    update: 优化学生注册过滤链

--- Alist-students/src/main/java/org/AList/service/handler/filter/register/StudentRegisterVerifyExistsByBloom.java
+++ Alist-students/src/main/java/org/AList/service/handler/filter/register/StudentRegisterVerifyExistsByBloom.java
@@ -1,18 +1,23 @@
 package org.AList.service.handler.filter.register;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.AList.common.convention.exception.UserException;
 import org.AList.domain.dto.req.StuRegisterReqDTO;
 import org.AList.service.bloom.StudentIdBloomFilterService;
+import org.springframework.stereotype.Component;
 
 import static org.AList.common.convention.errorcode.BaseErrorCode.USER_NOT_FOUND;
 @RequiredArgsConstructor
+@Component
+@Slf4j
 public final class StudentRegisterVerifyExistsByBloom implements StudentRegisterChainFilter<StuRegisterReqDTO>{
     private final StudentIdBloomFilterService studentIdBloomFilterService;
     @Override
     public void handler(StuRegisterReqDTO requestParam) {
+        log.info("id{}",requestParam.getStudentId());
         // 缓存全量学号的布隆过滤器提供快速错误机制 防止缓存穿透
-        if(!studentIdBloomFilterService.contain(requestParam.getStudentId())){
+        if(studentIdBloomFilterService.contain(requestParam.getStudentId())){
             throw new UserException(USER_NOT_FOUND);                                                                     //A0201：用户不存在
         }
     }
